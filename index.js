c575d39d6e57259640716391f1d06435b4afbe4d
Shahzeb Khan <shahzeb3939@gmail.com>
2020-09-27 00:18:32 +0500

    sync file read and write

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-var events = require('events')
-var util = require('util')
+var fs = require('fs')
 
-var Person = function(name){
-    this.name = name;
-}
-util.inherits(Person, events.EventEmitter)
-
-var personOne = new Person('kudra')
-var personTwo = new Person('yain')
-var personThree = new Person('tofu')
-
-persons = [personOne, personTwo, personThree]
-
-persons.forEach((person)=>{
-    person.on('speak', (mssg)=>{
-        console.log(mssg)
-    })
-})
-
-personOne.emit('speak', `${personOne.name}: How are you?`);
-personTwo.emit('speak', `${personTwo.name}: I am fine`);
-personThree.emit('speak', `${personThree.name}: Arry mujhe tu andar lo`);
-
-// console.log(personOne)
+var readMeContent = fs.readFileSync('readMe.txt', 'utf8');
+console.log(readMeContent)
+fs.writeFileSync('writtenFile.txt', readMeContent+' some more text')
