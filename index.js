7aaed1234ec4e22cadb23d3add557819fc4f51e2
Shahzeb Khan <shahzeb3939@gmail.com>
2020-09-26 20:12:41 +0500

    basic EventEmitter on and emit program

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var testModule = require('./testModule')
+var events = require('events')
 
-console.log(testModule.greetings('krishna'))
-console.log(testModule.hiking('jai'))
+var myEmitter = new events.EventEmitter();
+
+myEmitter.on('thisEvent', (first, second)=> {
+    console.log('this event was being emitted');
+    console.log(second);
+    console.log(`${first} - this was the first argument btw`)
+})
+
+myEmitter.emit('thisEvent', 'how will it take this', 'no chance for this second one')
