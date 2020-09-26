a78ec3e1a060cf4ed8036f59e5b1528bfdcb3f66
Shahzeb Khan <shahzeb3939@gmail.com>
2020-09-26 19:27:22 +0500

    a small initial code

---
+++ index.js
@@ -0,0 +1,4 @@
+var testModule = require('./testModule')
+
+console.log(testModule.greetings('krishna'))
+console.log(testModule.hiking('jai'))
---
+++ testModule.js
@@ -0,0 +1,9 @@
+
+
+module.exports.greetings = function (name) {
+    return 'welcome to the coding scene, ' + name;
+}
+
+module.exports.hiking = (name) => {
+    return `Now you are hiking, ${name}`
+}
