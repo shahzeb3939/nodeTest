d22c037602699a73279bac22151bccd7b5a5cd10
Shahzeb Khan <shahzeb3939@gmail.com>
2020-09-27 22:14:59 +0500

    displaying a simple html page using pipe - end of tutorial 17

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const { fstat } = require('fs')
 var http = require('http')
 var fs = require('fs')
 
-var myReadStream = fs.createReadStream(__dirname+'/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname+'/writeMe.txt')
+var server = http.createServer(function(req, res) {
+    console.log(`Request: ${req.url}`)
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    var myReadStream = fs.createReadStream(__dirname+'/index.html');
+    myReadStream.pipe(res)
+})
 
-myReadStream.pipe(myWriteStream);
-
-// var server = http.createServer(function(req, res) {
-//     console.log(`Request: ${req.url}`)
-//     res.writeHead(200, {'Content-Type': 'text/plane'})
-//     res.end("I now know how is this working")
-// })
-
-// server.listen(5000, '127.0.0.1');
-// console.log('listning to port 5000 on localhost 127.0.0.1')
+server.listen(5000, '127.0.0.1');
+console.log('listning to port 5000 on localhost 127.0.0.1')
